4fe44bcb8c14a74f28b0f3760737d9991fdf840b
ぽえぽえぽえぽえ <120158903+poepoepoepoe@users.noreply.github.com>
2023-11-07 12:51:01 +0900

    会員登録.html

---
+++ js/postcode.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ここにコードを書く
+
+// formを取得
+const contactForm = document.forms.contact;
+// formのpostcodeに郵便番号を入力したら関数を実行する
+contactForm.postcode.addEventListener('input', e => {
+  // zipcloud apiを使って、郵便番号の住所データを取得。
+  fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${e.target.value}`)
+    // 取得したデータをjson形式で読み込み。
+    .then(response => response.json())
+    // 取得したデータを出力
+    .then(data => {
+      contactForm.prefecture.value = data.results[0].address1 + data.results[0].address2;
+      contactForm.town.value = data.results[0].address3;
+    })
+    .catch(error => console.log(error))
+})
+
+});
+  
