2532c437c47068bb52f04c1fcb17132597a0d60a
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2023-11-17 11:51:25 +0900

    更なるリンク修正

--- main/product_detail.php
+++ main/product_detail.php
@@ -1,33 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="../css/search_result.css">
-		<link rel="stylesheet" href="../css/detail.css">
-		<title>Document</title>
-	</head>
-	<body>
-		<!-- header -->
-		<?php require 'header.php'; ?>
-		
-		<div class="content">
-			<div id="product_detail">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>カート一覧</title>
+	<link rel="stylesheet" href="../css/header.css">
+	<link rel="stylesheet" href="../css/product_detail.css">
+</head>
+<body>
+	<?php require 'header.php'; ?>
+	<div class="content">
+		<div id="product_detail">
+			<a href="#">カテゴリー</a><br>
+			<img src="../img/NoImage.png">
+			<div class="detail">
+				<h1 class="title">タイトル</h1>
+				<a href="#" class="store">△△△のストアを表示</a>
+				<p class="rating">4.0 ★★★★☆</p>
+				<a href="#">XX件の評価</a>
+				<h1 class="title">￥1000</h1>
 				<!-- PHP_START -->
-				<img class="detail" src="../img/SUGAKU.png">
-				<span class="name">す　う　が　く</span><br>
-				<span class="price">￥1300</span>
 				<!-- PHP_END -->
 			</div>
-			
-			<div id="price">
-				<span>合計金額</span>
-				<form action="purchased.php" method="POST">
-					<!-- PHP_START -->
-					<!-- PHP_END -->
-					<button type="submit">購入する</button>
-				</form>
-			</div>
 		</div>
-	</body>
+		
+		<div id="cost">
+			<form action="cart.php" method="POST">
+				<span class="yen">￥1000</span>
+				<p class="any">数量:<input type="number" class="number"></p>
+				<!-- PHP_START -->
+				<!-- PHP_END -->
+				<button type="submit">カートに入れる</button>
+			</form>
+		</div>
+	</div>
+</body>
 </html>
