ca53b5140b06f6e3c60a9f5eacc665a805539b19
TMY-0705 <148175126+TMY-0705@users.noreply.github.com>
2023-11-30 11:58:50 +0900

    test

--- control/goods_delete.php
+++ control/goods_delete.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+	<?php require '../php_init/db-connect.php' ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>削除確認</title>
@@ -9,8 +10,22 @@
 </head>
 <body>
 	<?php require 'header.php' ?>
+	<?php
+	$id = $_GET['id'];
+	$sql = $db->query(
+		"SELECT * FROM Products
+			JOIN Categories
+			ON Products.category_id = Categories.category_id
+			WHERE Products.product_id = $id
+		"
+	);
+	$res = $sql->fetch(PDO::FETCH_ASSOC);
+
+	$sql = $db->query("SELECT COUNT(product_id) as cnt, AVG(history_detail_rate) as avg FROM Histories_detail");
+	$res2 = $sql->fetch(PDO::FETCH_ASSOC);
+?>
     <p class="sakujo">削除しますか？</p>
-    <button class="shohin"><img class="img1" src="../img/NoImage.png">
+    <button class="shohin"><img class="img1" src='../img/<?= $res['product_image'] ?>' alt='<?= $res['product_image'] ?>の画像がでてナイ！'>
 	<table class="itiran">
 		<tr>
 			<td>メーカー：ＸＸＸ</td>
