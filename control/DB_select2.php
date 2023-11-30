cf7abde1ee46eaec752211148874edace343a3b0
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2023-12-01 01:05:38 +0900

    コミットし忘れ

--- control/DB_select2.php
+++ control/DB_select2.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DB選択</title>
-    <link rel="stylesheet" href="../css/header1.css">
-    <link rel="stylesheet" href="../css/DB_select.css">
+    <link rel="stylesheet" href="../css/header4.css">
+    <link rel="stylesheet" href="../css/DB_select2.css">
 </head>
 <!-- header1 -->
 <body>
@@ -14,7 +14,7 @@
 	<?php require '../php_init/db-connect.php' ?>
 	<?php
 	
-	$sql=$db->query('select * from Products');
+	$sql=$db->query('SELECT * FROM Products JOIN Categories ON Products.category_id = Categories.category_id');
 	foreach($sql as $row){
 
 		echo '<button class="shohin" onclick="location.href=\'rate_check.php\'">';
@@ -22,10 +22,14 @@
 		echo '<table class="itiran">';
 			echo '<tr>';
 				echo '<td class="text">メーカー：', $row['product_maker'], '</td>';
-				echo '<td class="text">カテゴリー：', $row['category_id'], '</td>';
+				echo '<td class="text">カテゴリー：', $row['category_name'], '</td>';
 			echo '</tr>';
 			echo '<tr>';
-				echo '<td class="text">商品名：', $row['product_name'], '</td>';
+				if(strlen($row['product_name'])>10) {
+					echo '<td class="text">商品名：', mb_substr($row['product_name'], 0, 10), '…</td>';
+				} else {
+					echo '<td class="text">商品名：', $row['product_name'], '</td>';
+				}
 				echo '<td class="text">値段：', $row['product_price'], '</td>';
 				echo '<td class="text">在庫数：', $row['product_stock'], '</td>';
 			echo '</tr>';
