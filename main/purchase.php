f7d0efb54633d4712c35895bbe90e7751889c74b
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2023-12-01 12:36:30 +0900

    購入処理（ハリボテ）の追加

--- main/purchase.php
+++ main/purchase.php
@@ -1,4 +1,17 @@
 <?php require '../php_init/login_check.php' ?>
-<?php 
-	
+<?php require '../php_init/db-connect.php' ?>
+<?php
+	$acc_id = $_SESSION['loginfo']['acc_id'];
+	$today = date("Ymd");
+	try {
+		$sql = $db -> query("INSERT INTO Histories VALUE (NULL, $acc_id, $today)");
+		
+	} catch (PDOException $e) {
+		echo $e;
+		header("Location: cart.php?err=$err", true, 307);
+	} catch (Exception $e) {
+		echo '<h2>通常の例外発生！！！<br>', $e, "</h2>";
+	} catch (Throwable $e) {
+		echo '<h2>特殊な例外発生！！！<br>', $e, "</h2>";
+	}
 ?>
