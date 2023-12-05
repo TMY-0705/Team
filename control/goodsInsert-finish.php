7ece34803c995b453a3cf2de1175b620762df8ee
ぽえぽえぽえぽえ <120158903+poepoepoepoe@users.noreply.github.com>
2023-12-05 12:15:12 +0900

    ins

--- control/goodsInsert-finish.php
+++ control/goodsInsert-finish.php
@@ -10,21 +10,31 @@
 <!-- header1.php -->
 <body>
 	<?php require 'header2.php' ?>
-    <h1>登録しました</h1>
-    <button class="shohin"><img class="img1" src="../img/SUGAKU.png">
+	<?php require '../php_init/db-connect.php' ?>
+	<?php
+	$sql=$db->prepare('insert into Products Values (NULL, ?, ?, ?, ?, ?, ?)');
+	$sql->execute([$_POST["pname"], $_POST["price"], $_POST["stock"], $_FILES['upload_image']['name'], $_POST["mname"], $_POST["category"]]);
 
-	<table class="itiran">
-		<tr>
-			<td>メーカー：ＸＸＸ</td>
-			<td>カテゴリー：ＸＸＸ</td>
-		</tr>
-		<tr>
-			<td>商品名：ＸＸＸ</td>
-			<td>値段：ＸＸＸ</td>
-			<td>在庫数：ＸＸＸ</td>
-		</tr>
-	</table>
-    
+	$filename = $_FILES['upload_image']['name'];
+	$uploaded_path = '../img/'.$filename;
+	$result = move_uploaded_file($_FILES['upload_image']['tmp_name'],$uploaded_path);
+
+    echo '<h1>登録しました</h1>';
+
+    echo '<button class="shohin"><img class="img1" src="../img/'.$_FILES['upload_image']['name']. '">';
+
+	echo '<table class="itiran">';
+		echo '<tr>';
+			echo '<td>メーカー：', $_POST['mname'], '</td>';
+			echo '<td>カテゴリー：', $_POST['category'], '</td>';
+		echo '</tr>';
+		echo '<tr>';
+			echo '<td>商品名：', $_POST['pname'], '</td>';
+			echo '<td>値段：', $_POST['price'], '</td>';
+			echo '<td>在庫数：', $_POST['stock'], '</td>';
+		echo '</tr>';
+	echo '</table>';
+    ?>
 	</button><br><br><br>
     <input type="button" class="button" onclick="location.href='goodsInsert.php'" value="続けて登録する">
     
