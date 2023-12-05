e044e42f611af811c9b0921d2438727c719b14b6
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2023-12-05 11:18:52 +0900

    いろいろ修正

--- main/account_create.php
+++ main/account_create.php
@@ -10,14 +10,14 @@
 </head>
 <body>
 	<?php
-		$name       = $_POST['name']       ?? $res['account_name'];
-		$mail       = $_POST['mail']       ?? $res['account_email'];
+		$name       = $_POST['name']       ?? null;
+		$mail       = $_POST['mail']       ?? null;
 		$pass1      = "";
 		$pass2      = "";
-		$postcode   = $_POST['postcode']   ?? $res['account_postal'];
-		$prefecture = $_POST['prefecture'] ?? $res['account_addr_main'];
-		$town       = $_POST['town']       ?? $res['account_addr_detail'];
-		$house      = $_POST['house']      ?? $res['account_addr_building'];
+		$postcode   = $_POST['postcode']   ?? null;
+		$prefecture = $_POST['prefecture'] ?? null;
+		$town       = $_POST['town']       ?? null;
+		$house      = $_POST['house']      ?? null;
 	?>
 
 	<form name="contact" method="POST" action="creating.php">    
