f787aadf68585bf2124114fa7cf93c20dda82d87
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2023-12-05 12:16:40 +0900

    カートとかもろもろ

--- control/goodsInsert-finish.php
+++ control/goodsInsert-finish.php
@@ -10,21 +10,26 @@
 <!-- header1.php -->
 <body>
 	<?php require 'header2.php' ?>
-    <h1>登録しました</h1>
-    <button class="shohin"><img class="img1" src="../img/SUGAKU.png">
+	<?php require '../php_init/db-connect.php' ?>
+	<?php
+	$sql=$db->prepare('insert into Products Values (NULL, ?, ?, ?, ?, ?, ?)');
+	$sql->execute([$_GET["pname"], $_GET["price"], $_GET["stock"], $_GET["image"], $_GET["mname"], $_GET["category"]]);
+    echo '<h1>登録しました</h1>';
 
-	<table class="itiran">
-		<tr>
-			<td>メーカー：ＸＸＸ</td>
-			<td>カテゴリー：ＸＸＸ</td>
-		</tr>
-		<tr>
-			<td>商品名：ＸＸＸ</td>
-			<td>値段：ＸＸＸ</td>
-			<td>在庫数：ＸＸＸ</td>
-		</tr>
-	</table>
-    
+    echo '<button class="shohin"><img class="img1" src="../img/'.$_POST['image']. '">';
+
+	echo '<table class="itiran">';
+		echo '<tr>';
+			echo '<td>メーカー：', $_POST['mname'], '</td>';
+			echo '<td>カテゴリー：', $_POST['category'], '</td>';
+		echo '</tr>';
+		echo '<tr>';
+			echo '<td>商品名：', $_POST['pname'], '</td>';
+			echo '<td>値段：', $_POST['price'], '</td>';
+			echo '<td>在庫数：', $_POST['stock'], '</td>';
+		echo '</tr>';
+	echo '</table>';
+    ?>
 	</button><br><br><br>
     <input type="button" class="button" onclick="location.href='goodsInsert.php'" value="続けて登録する">
     
--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,19 @@
+var defaultCost = null;
+
+function defaultCost() {
+	defaultCost = document.getElementById("total").innerHTML;
+}
+
 function recalc(){
 	var num = 0;
 
 	for(var i=0; i<n; i++){
 		var x = document.getElementById("price_"+i);
 		var y = document.getElementById("amount_"+i);
-		console.log(x.value + ", " + y.value);
 
 		num += x.value * y.value;
 	}
 
 	var z = document.getElementById("total");
-	z.innerHTML = "￥" + (num).toLocaleString();
+	z.innerHTML = defaultCost+"\n　　↓"+"\n￥" + (num).toLocaleString();
 }
