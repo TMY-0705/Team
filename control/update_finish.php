c1b42676f7fd4e6047e272b1e5d4bdef4f455e03
TK0606 <2201137@s.asojuku.ac.jp>
2023-12-06 11:19:41 +0900

    update

--- control/update_finish.php
+++ control/update_finish.php
@@ -14,7 +14,7 @@
 	<?php
 	$id = $_GET['id'];
 	$filename = $_FILES['upload_image']['name'];
-	$sql=$db->prepare('update Products set product_name=?,product_price=?,product_stock=?,product_image=?,product_maker=?,category_id=? WHERE product_id=?)');
+	$sql=$db->prepare('update Products set product_name=?,product_price=?,product_stock=?,product_image=?,product_maker=?,category_id=? WHERE product_id=?');
 	$sql->execute([$_POST["pname"], $_POST["price"], $_POST["stock"], $filename, $_POST["mname"], $_POST["category"],$id]);
 
 	
