c4af9eecf4c4841707344b3df3b719a05b66a36d
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2023-12-13 12:24:06 +0900

    名前が変わってらぁ

--- main/products.php
+++ main/products.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="../css/search_result.css">
+		<link rel="stylesheet" href="../css/products.css">
 		<?php
 			$name = $_GET['key'] ?? null;
 			$maker = $_GET['maker'] ?? null;
